ee913b57b414510cef1ba395129c73556af2e05b
Areeb Sheikh <as3148300@gmail.com>
2026-09-24 03:06:01 +0530

    Register done!

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,9 +2,9 @@ const express = require("express")
 const userModel = require("../models/users.model")
 const authRouter = express.Router()
 const crypto = require("crypto")
-
+const jwt = require("jsonwebtoken")
 authRouter.post("/register", async (req, res) => {
-    const { userName, email, phoneNumber, profilePic, bio } = req.body
+    const { userName, email, phoneNumber, profilePic, bio , password } = req.body
     const userAlreadyExits = await userModel.findOne({
         $or: [
             { userName },
@@ -17,14 +17,31 @@ authRouter.post("/register", async (req, res) => {
         })
     }
 
-    const hash = crypto.createHash("sha256").update(password).digest("hex")
+const hash = crypto.createHash("sha256").update(password).digest("hex")
 
     const user = await userModel.create({
         userName,
         email,
         bio,
         profilePic,
+        phoneNumber,
         password: hash
     })
+ 
+    const token = jwt.sign({
+        id:user._id
+    },process.env.JWT_SECRET,{expiresIn:"1d"})
+    res.cookie("token" , token)
 
+    res.status(409).json({
+        message:"user successfully registered!!",
+        user:{
+            email:user.email,
+            username:user.userName,
+            bio:user.bio,
+            profilePic:user.profilePic,
+            phoneNumber:user.phoneNumber
+        }
+    })
 })
+module.exports = authRouter
